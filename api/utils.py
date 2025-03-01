9931d3dc1611a326fc5f873d9020acc83d196752
Piotr Orlowski <p.orlowski1@gmail.com>
2025-03-01 19:28:45 +0100

    remove unnecessary comment

--- api/utils.py
+++ api/utils.py
@@ -18,7 +18,6 @@ def find_index(value: int, numbers: list[int]) -> Union[int, None]:
         mid = (left + right) // 2
         mid_value = numbers[mid]
 
-        # If mid_value is within the 10% range, check if it's closer to the target
         if lower_bound <= mid_value <= upper_bound:
             diff = abs(mid_value - value)
             if closest_diff is None or diff < closest_diff:
